20b6e49e6039955e1b88b10622d7cc705ba9420d
Kanomtom-aroi <salami.learn@gmail.com>
2025-03-23 22:59:17 +0700

    fix some func

--- src/libs/getHotel.tsx
+++ src/libs/getHotel.tsx
@@ -1,7 +1,8 @@
 export default async function getHotel(id:string) {
     const response = await fetch(`https://cozyhotel-be.vercel.app/api/v1/hotels/${id}`);
-  if (!response.ok) {
-    throw new Error("Failed to fetch Hotel");
-  }
+    if (!response.ok) {
+      throw new Error("Failed to fetch Hotels");
+    }
+  
   return await response.json(); 
 }
