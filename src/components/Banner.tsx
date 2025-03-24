9e5db3a4d1b4ab983f1ba91aed0cc696011909cb
Ittyy <96482477+Ittyy-Winter@users.noreply.github.com>
2025-03-24 13:36:33 +0700

    Revamp About page layout and content, enhancing visual appeal and user engagement with detailed sections on the company's story, mission, and offerings. Update Banner component to feature a video background for a more dynamic presentation.

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,37 +1,25 @@
 "use client";
-import Image from "next/image";
 import { useState } from "react";
 import { useRouter } from "next/navigation";
 import { useSession } from "next-auth/react";
 
 export default function Banner() {
-  const covers = ["/img/cover.jpg", "/img/cover2.jpg", "/img/cover3.jpg"];
-  const [index, setIndex] = useState(0);
   const router = useRouter();
-
   const { data: session } = useSession();
-  console.log(session?.user.token);
 
   return (
-    <div
-      className="block w-screen h-[80vh] relative p-1 m-0"
-      onClick={() => {
-        setIndex(index + 1);
-      }}
-    >
-      <Image
-        src={covers[index % 3]}
-        alt="cover"
-        fill={true}
-        priority
-        objectFit="cover"
-        style={{ filter: "brightness(50%)" }}
-      />
+    <div className="block w-screen h-[80vh] relative p-1 m-0">
+      <video
+        autoPlay
+        loop
+        muted
+        playsInline
+        className="absolute top-0 left-0 w-full h-full object-cover brightness-50"
+      >
+        <source src="https://media.iceportal.com/138346/Videos/video040524214954517_1080p.mp4" type="video/mp4" />
+      </video>
       <div
-        className="relative top-[25%] z-20 text-center"
-        onClick={(e) => {
-          e.stopPropagation();
-        }}
+        className="relative top-[35%] z-20 text-center"
       >
         <h1 className="text-4xl font-serif text-white">CozyHotel</h1>
         <h3 className="text-xl font-serif text-white">
@@ -41,19 +29,18 @@ export default function Banner() {
       {session ? (
         <div
           className="z-30 absolute top-5 right-10 font-serif text-white text-xl"
-          onClick={(e) => {
-            e.stopPropagation();
-          }}
         >
           Hello {session.user?.name}
         </div>
       ) : null}
       <button
-        className="bg-[#181A1B] text-[#52D7F7] border border-[#52D7F7] 
-        font-semibold py-2 px-4 m-2 rounded z-30 absolute bottom-2 right-5 hover:bg-[#52D7F7]
-        hover:text-[#181A1B] hover:boarder-transparent"
-        onClick={(e) => {
-          e.stopPropagation();
+        className="bg-black text-white font-serif 
+             py-3 px-6 rounded-full z-30 absolute bottom-5 right-5 
+             transition-all duration-300 ease-in-out
+             border border-[#60788B] shadow-lg
+             hover:bg-gray-400 hover:border-[#8FA7BE] 
+             hover:text-white transform hover:scale-105"
+        onClick={() => {
           router.push("/hotel");
         }}
       >
